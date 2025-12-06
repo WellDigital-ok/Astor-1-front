27064726b1ff45849cccaad6ed977e428e00b12b
Joaquin Bidart <joaquinbidart19@gmail.com>
2025-12-06 21:45:28 +0000

    necesito que cuando se comparta el link de la pagina por wpp aparezca co

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,22 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: "Astor's Bash",
-  description: "You're invited to Astor's birthday bash!",
+  title: 'Astor Cumple 1 !',
+  description: 'Te esperamos para compartir un día extraordinario , no faltes!',
+  openGraph: {
+    title: 'Astor Cumple 1 !',
+    description: 'Te esperamos para compartir un día extraordinario , no faltes!',
+    images: [
+      {
+        url: '/metadata.jpeg',
+        width: 1200,
+        height: 630,
+        alt: 'Invitación al cumpleaños de Astor',
+      },
+    ],
+    locale: 'es_AR',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
