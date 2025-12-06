95fc7000d0d0fecf7a04f4b9f832d0961f64b6d9
Joaquin Bidart <joaquinbidart19@gmail.com>
2025-12-06 23:11:49 +0000

    necesito que cuando se comparta el link de la pagina por wpp aparezca co

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pageUrl = process.env.NEXT_PUBLIC_API_URL
+    ? `https://${process.env.NEXT_PUBLIC_API_URL.split('/api')[0]}`
+    : '';
+
   return (
     <html lang="en">
       <head>
@@ -40,6 +44,14 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
+        <meta property="og:title" content="Astor Cumple 1 !" />
+        <meta
+          property="og:description"
+          content="Te esperamos para compartir un día extraordinario , no faltes!"
+        />
+        <meta property="og:image" content={`${pageUrl}/metadata.jpeg`} />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="og:type" content="website" />
       </head>
       <body className="font-body antialiased">
         <ApiProvider>
